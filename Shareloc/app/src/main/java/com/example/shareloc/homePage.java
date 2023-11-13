cb242b6fdc042b0b9c21b3219e3b64d256162a3d
LouiseESME <louise.martin.ndv@free.fr>
2023-11-13 17:24:03 +0100

    creation page d'accueil

---
+++ Shareloc/app/src/main/java/com/example/shareloc/homePage.java
@@ -0,0 +1,15 @@
+package com.example.shareloc;
+
+        import android.view.View;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+
+        import java.security.SecureRandom;
+
+public class homePage extends AppCompatActivity implements View.OnClickListener {
+
+}
