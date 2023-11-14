532ea1f60da3d0c31b8056ffaa691abdfe84b2a2
LouiseESME <louise.martin.ndv@free.fr>
2023-11-14 14:40:46 +0100

    modif pour acceder a la deuxieme page

--- Shareloc/app/src/main/java/com/example/shareloc/homePage.java
+++ Shareloc/app/src/main/java/com/example/shareloc/homePage.java
@@ -1,6 +1,8 @@
 package com.example.shareloc;
 
         import android.view.View;
+
+        import androidx.annotation.Nullable;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
@@ -11,5 +13,15 @@ package com.example.shareloc;
         import java.security.SecureRandom;
 
 public class homePage extends AppCompatActivity implements View.OnClickListener {
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.home_page);
+        }
 
+        @Override
+        public void onClick(View v) {
+
+        }
 }
+
