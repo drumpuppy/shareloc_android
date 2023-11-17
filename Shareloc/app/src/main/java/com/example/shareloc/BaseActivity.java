5eba72c8d033deeb93bfd9b88e41da7f50e0a71c
etienne <etienne.lazarz@mytower.co>
2023-11-17 16:11:53 +0100

    fix logout

--- Shareloc/app/src/main/java/com/example/shareloc/BaseActivity.java
+++ Shareloc/app/src/main/java/com/example/shareloc/BaseActivity.java
@@ -10,9 +10,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.auth.FirebaseUser;
+
 
 public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected DrawerLayout drawer;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,28 @@ public abstract class BaseActivity extends AppCompatActivity implements Navigati
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            redirectToLogin();
+        }
 
         toggle.syncState();
     }
 
     protected abstract int getLayoutId();
 
+    private void logout() {
+        mAuth.signOut();
+        redirectToLogin();
+    }
+
+    private void redirectToLogin() {
+        Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -44,6 +66,8 @@ public abstract class BaseActivity extends AppCompatActivity implements Navigati
             startActivity(new Intent(this, homePage.class));
         } else if (id == R.id.nav_amis && !(this instanceof AmisActivity)) {
             startActivity(new Intent(this, AmisActivity.class));
+        } else if (id == R.id.nav_logout) {
+            logout();
         }
         // Add similar checks for other navigation items
 
--- Shareloc/app/src/main/java/com/example/shareloc/MainActivity.java
+++ Shareloc/app/src/main/java/com/example/shareloc/MainActivity.java
@@ -9,23 +9,15 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.activity.OnBackPressedCallback;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import androidx.core.view.GravityCompat;
 
 public class MainActivity extends BaseActivity {
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mAuth = FirebaseAuth.getInstance();
 
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser == null) {
-            redirectToLogin();
-        }
 
         // Handle back pressed for the navigation drawer
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
@@ -55,20 +47,8 @@ public class MainActivity extends BaseActivity {
         return R.layout.activity_main;
     }
 
-    private void logout() {
-        mAuth.signOut();
-        redirectToLogin();
-    }
-
     private void openHomePage() {
         Intent intent = new Intent(MainActivity.this, homePage.class);
         startActivity(intent);
     }
-
-    private void redirectToLogin() {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
 }
