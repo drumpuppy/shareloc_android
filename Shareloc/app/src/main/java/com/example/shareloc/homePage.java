6c69b5b51dda36969866df01a3288cdfc86cc92f
etienne <etienne.lazarz@mytower.co>
2023-11-17 19:13:38 +0100

    added details

--- Shareloc/app/src/main/java/com/example/shareloc/homePage.java
+++ Shareloc/app/src/main/java/com/example/shareloc/homePage.java
@@ -1,19 +1,81 @@
-
 package com.example.shareloc;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
 
-public class homePage extends BaseActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.Manifest;
+
+
+public class homePage extends BaseActivity implements OnMapReadyCallback {
+
+        private GoogleMap mMap;
+        private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, new String[]{
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        }, LOCATION_PERMISSION_REQUEST_CODE);
+                }
+
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                if (mapFragment != null) {
+                        mapFragment.getMapAsync(this);
+                }
+
+
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+                        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                                if (mMap != null) {
+                                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                                // TODO: Consider calling
+                                                //    ActivityCompat#requestPermissions
+                                                // here to request the missing permissions, and then overriding
+                                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                                //                                          int[] grantResults)
+                                                // to handle the case where the user grants the permission. See the documentation
+                                                // for ActivityCompat#requestPermissions for more details.
+                                                return;
+                                        }
+                                        mMap.setMyLocationEnabled(true);
+                                        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                                }
+                        }
+                }
         }
 
         @Override
         protected int getLayoutId() {
                 return R.layout.home_page;
         }
-}
 
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                        mMap.getUiSettings().setZoomControlsEnabled(true);
+                }
+        }
+}
