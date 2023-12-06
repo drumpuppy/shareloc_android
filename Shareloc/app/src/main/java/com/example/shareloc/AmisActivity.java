d7e6058089a76a29929289868e6377147b4d3d51
LouiseESME <louise.martin.ndv@free.fr>
2023-12-06 22:56:50 +0100

    modif du menu déroulant

--- Shareloc/app/src/main/java/com/example/shareloc/AmisActivity.java
+++ Shareloc/app/src/main/java/com/example/shareloc/AmisActivity.java
@@ -3,11 +3,16 @@ package com.example.shareloc;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class AmisActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
     }
 
     @Override
