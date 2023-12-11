327e8c6723fa5d8f045bdc1d4eff732e8ec1aa5d
hanks92 <146184927+hanks92@users.noreply.github.com>
2023-12-11 14:24:35 +0100

    Add files via upload

--- Shareloc/app/src/main/java/com/example/shareloc/ApiManager.java
+++ Shareloc/app/src/main/java/com/example/shareloc/ApiManager.java
@@ -1,5 +1,7 @@
 package com.example.shareloc;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -18,14 +20,35 @@ public class ApiManager {
     public void updateUser(String userId, User user) {
         usersRef.child(userId).setValue(user);
     }
+
     public void getUser(String userId, ValueEventListener listener) {
         usersRef.child(userId).addListenerForSingleValueEvent(listener);
     }
 
-    public void addFriend(String userId, String friendId) {
-        usersRef.child(userId).child("friends").push().setValue(friendId);
-    }
+    public void addFriend(String userId, final String friendId, final OnFriendAddedListener listener) {
+        usersRef.child(friendId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    usersRef.child(userId).child("friends").push().setValue(friendId);
+                    if (listener != null) {
+                        listener.onFriendAdded(true);
+                    }
+                } else {
+                    if (listener != null) {
+                        listener.onFriendAdded(false);
+                    }
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                if (listener != null) {
+                    listener.onFriendAdded(false);
+                }
+            }
+        });
+    }
 
     public void removeFriend(String userId, String friendId) {
         usersRef.child(userId).child("friends").child(friendId).removeValue();
@@ -35,4 +58,7 @@ public class ApiManager {
         usersRef.child(userId).child("friends").addListenerForSingleValueEvent(listener);
     }
 
+    public interface OnFriendAddedListener {
+        void onFriendAdded(boolean success);
+    }
 }
